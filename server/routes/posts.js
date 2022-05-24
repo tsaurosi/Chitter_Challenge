1b5743d09078c7e4c00afc1e9732e74314162f61
Tadi Saurosi <96049919+tsaurosi@users.noreply.github.com>
2022-05-24 21:33:39 +0100

    front end working and server built

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { getPosts, createPost} from '../controllers/posts.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router;
+
+
+
+// http://localhost:5000/posts
